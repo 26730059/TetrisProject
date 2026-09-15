1ec7ead4ebc6985cf0d2adf9dc85cdbf2dc8f0a7
thupm12 <26730067@ms.uit.edu.vn>
2026-09-15 21:41:21 +0700

    feat: Load and unload sound effects (SFX) in Init/Cleanup

--- src/SoundManager.cpp
+++ src/SoundManager.cpp
@@ -2,8 +2,23 @@
 
 void SoundManager::Init() {
     InitAudioDevice();
+
+    // Load am thanh hieu ung
+    sfxRotate   = LoadSound("assets/sound/rotation.mp3");
+    sfxDrop     = LoadSound("assets/sound/drop.mp3");
+    sfxHold     = LoadSound("assets/sound/hold.mp3");
+    sfxClear    = LoadSound("assets/sound/line_clear.mp3");
+    sfxClear4   = LoadSound("assets/sound/tetris_clear.mp3");
+    sfxGameOver = LoadSound("assets/sound/game_over.mp3");
 }
 
 void SoundManager::Cleanup() {
+    UnloadSound(sfxRotate);
+    UnloadSound(sfxDrop);
+    UnloadSound(sfxHold);
+    UnloadSound(sfxClear);
+    UnloadSound(sfxClear4);
+    UnloadSound(sfxGameOver);
+
     CloseAudioDevice();
 }
