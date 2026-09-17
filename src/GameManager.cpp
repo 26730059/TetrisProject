663c83044a38dea781a4e063238adbb63050d0e4
Thảo My Hoàng <26730049@ms.uit.edu.vn>
2026-09-17 16:45:52 +0700

    feat: init window and start main loop

--- src/GameManager.cpp
+++ src/GameManager.cpp
@@ -1,6 +1,19 @@
 #include "GameManager.h"
 
-void GameManager::Init() {}
+void GameManager::Init() {
+    InitWindow(SCREEN_W, SCREEN_H, "Tetris - raylib");
+    SetTargetFPS(60);
+}
+
 void GameManager::ResetGame() {}
-void GameManager::Run() {}
-void GameManager::Cleanup() {}
+
+void GameManager::Run() {
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        EndDrawing();
+    }
+}
+
+void GameManager::Cleanup() {
+    CloseWindow();
+}
