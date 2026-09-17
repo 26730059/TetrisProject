f04b0711e5d13782401d95b7191eeb89e425e3c2
Thảo My Hoàng <26730049@ms.uit.edu.vn>
2026-09-17 16:46:47 +0700

    feat: reset game logic on startup

--- src/GameManager.cpp
+++ src/GameManager.cpp
@@ -3,9 +3,13 @@
 void GameManager::Init() {
     InitWindow(SCREEN_W, SCREEN_H, "Tetris - raylib");
     SetTargetFPS(60);
+
+    ResetGame();
 }
 
-void GameManager::ResetGame() {}
+void GameManager::ResetGame() {
+    logic.Reset();
+}
 
 void GameManager::Run() {
     while (!WindowShouldClose()) {
